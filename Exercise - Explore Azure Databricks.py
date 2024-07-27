ccd6bc6eaf2421e5ef229689d25d1b4a584e11ff
github-dask <github-dask>
2024-07-27 13:46:38 +0000

    Commit 1

---
+++ Exercise - Explore Azure Databricks.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Follow steps from the MS Leraning Exercise
+# MAGIC https://microsoftlearning.github.io/mslearn-databricks/Instructions/Exercises/01-Explore-Azure-Databricks.html
+# MAGIC
+# MAGIC It requires Github:
+# MAGIC git clone https://github.com/MicrosoftLearning/mslearn-databricks
+# MAGIC
+# MAGIC And access to Free data from :
+# MAGIC https://raw.githubusercontent.com/MicrosoftLearning/mslearn-databricks/main/data/products.csv
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM `hive_metastore`.`default`.`products`;
+
+# COMMAND ----------
+
+ df = spark.sql("SELECT * FROM products")
+ df = df.filter("Category == 'Road Bikes'")
+ display(df)
